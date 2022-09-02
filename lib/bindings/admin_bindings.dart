a0bef3c0b588af801825e1e44d1e447b74fd2da9
Seeeev <sevenabante9@gmail.com>
2022-09-02 08:35:21 +0800

    removed some errors

--- lib/bindings/admin_bindings.dart
+++ lib/bindings/admin_bindings.dart
@@ -2,12 +2,10 @@ import 'package:get/get.dart';
 import 'package:library_management/controllers/admin_controller/table_controller.dart';
 import 'package:library_management/controllers/admin_controller/update_book_controller.dart';
 
-class AdminBindings extends Bindings{
+class AdminBindings extends Bindings {
   @override
   void dependencies() {
     // TODO: implement dependencies
     Get.put(UpdateBookController());
-    Get.put(TableController());
   }
-
 }
